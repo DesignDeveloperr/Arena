873c90424202ee7a0aba45c5352c99bb5eee639f
Danila <danila.anfertiev3@gmail.com>
2022-03-25 06:28:35 +0600

    add road

--- js/index.js
+++ js/index.js
@@ -6,7 +6,17 @@
             zoom: 20
         });
 
-        DG.marker([54.96812, 73.38274]).addTo(map);
+        DG.marker([54.96812, 73.38274]).addTo(map).bindLabel('Мы здесь!', {
+            static: true
+        });
+
+        const coordinates = [[54.9691, 73.383239], [54.9683, 73.38358], [54.96824, 73.383229]];
+
+        const polyline = DG.polyline(coordinates, {
+            color: 'red'
+        }).addTo(map);
+
+        map.fitBounds(polyline.getBounds());
     });
 
     const images = document.querySelectorAll('img')
